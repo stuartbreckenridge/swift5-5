fc786903df5e4a0d9ef3ae84c98c1d635659ba62
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2021-05-29 16:56:26 +0800

    Initial Commit

---
+++ swift5-5/swift5_5App.swift
@@ -0,0 +1,17 @@
+//
+//  swift5_5App.swift
+//  swift5-5
+//
+//  Created by Stuart Breckenridge on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift5_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
